2f52f4c8f49fb11a6a19104b4caf554c0fe2ab5f
VLx2000 <victor.antunes.2000@gmail.com>
2022-04-05 22:56:15 -0300

    adicionando scroll infinito

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import PokemonCard from "../components/PokemonCard";
 import './Home.css';
@@ -6,16 +6,42 @@ import './Home.css';
 function Home() {
 
     const [lista_pokemon, setLista_pokemon] = useState('undefined');
+    const [currentPage, setCurrentPage] = useState(1);
+    const loaderRef = useRef(null);
 
     useEffect(() => {
         axios
-            .get(`${process.env.REACT_APP_API_URL}pokemon?offset=0&limit=102`)
+            .get(`${process.env.REACT_APP_API_URL}pokemon`, {
+                params: {
+                  limit: currentPage * 12,
+                }
+            })
             .then(response => {
                 const data = response.data;
                 //console.log(data);
                 setLista_pokemon(data);
             });
-    }, [])
+    }, [currentPage]);
+
+    useEffect(() => {
+        const options = {
+          root: null,
+          rootMargin: "20px",
+          threshold: 1.0
+        };
+    
+        const observer = new IntersectionObserver((entities) => {
+          const target = entities[0];
+    
+          if (target.isIntersecting){
+            setCurrentPage(old => old + 1);
+          }
+        }, options);
+    
+        if (loaderRef.current){
+          observer.observe(loaderRef.current);
+        }
+      }, []);
     
     return(
         <div id="home">
@@ -29,6 +55,7 @@ function Home() {
                     :
                         <div>Carregando...</div>
                     }
+                    <p ref={loaderRef}>Carregando...</p>
                 </div>
             </div>
         </div>
