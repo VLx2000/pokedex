fd3a6f4facee69d24321f78232bca877a0df4898
VLx2000 <victor.antunes.2000@gmail.com>
2022-04-13 11:23:14 -0300

    mudança nas cores

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -17,7 +17,7 @@ function PokemonCard(props) {
             .get(pokemon.url)
             .then(response => {
                 const data = response.data;
-                console.log(data);
+                //console.log(data);
                 setAtributos(data);
                 setColor(data.types[0].type.name)
             })
@@ -46,7 +46,6 @@ function PokemonCard(props) {
         return {
             backgroundColor: padrao(color),
             color: contrastada(color),
-            boxShadow: "0 0 10px 3px " + desaturada(color),
         }
     }
 
@@ -54,32 +53,18 @@ function PokemonCard(props) {
         <>
             {(typeof atributos != 'undefined') ?
                 <div>
-                    <div className="frente" style={virar ? { display: 'none' } : { display: 'block' }}>
-                        <div className="card" key={1} style={styleCard} onClick={() => setVirar(!virar)}>
-                            <p className="topCard">
-                                <span className="nome" style={styleNome}>{pokemon.name}</span>
-                                <span className="id" style={styleId}>#{atributos.id}</span>
-                            </p>
-                            <img src={atributos.sprites.front_default} alt={atributos.sprites.other["official-artwork"].front_d}/>
-                            <p className="tipos">
-                                {(atributos.types.map(types =>
-                                    <span className="tipo" key={types.type.name}
-                                        style={styleTipo(types.type.name)}>{types.type.name}</span>
-                                ))}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="verso" style={virar ? { display: 'block' } : { display: 'none' }}>
-                        <div className="card" key={2} style={styleCard} onClick={() => setVirar(!virar)}>
-                            <span>Peso: {atributos.weight} </span>
-                            <span>Altura: {atributos.height} </span>
-                            <p>{atributos.stats[0].base_stat}hp</p>
-                            <p>{atributos.stats[1].base_stat}ataque</p>
-                            <p>{atributos.stats[2].base_stat}defesa</p>
-                            <p>{atributos.stats[3].base_stat}ataque especial</p>
-                            <p>{atributos.stats[4].base_stat}defesa especial</p>
-                            <p>{atributos.stats[5].base_stat}velocidade</p>
-                        </div>
+                    <div className="card" key={1} style={styleCard} onClick={() => setVirar(!virar)}>
+                        <p className="topCard">
+                            <span className="nome" style={styleNome}>{pokemon.name}</span>
+                            <span className="id" style={styleId}>#{atributos.id}</span>
+                        </p>
+                        <img src={atributos.sprites.front_default} alt={atributos.sprites.other["official-artwork"].front_d} />
+                        <p className="tipos">
+                            {(atributos.types.map(types =>
+                                <span className="tipo" key={types.type.name}
+                                    style={styleTipo(types.type.name)}>{types.type.name}</span>
+                            ))}
+                        </p>
                     </div>
                 </div>
                 :
--- src/pages/Specs.jsx
+++ src/pages/Specs.jsx
@@ -28,9 +28,18 @@ function Specs(props) {
 		<div id="specs">
 			<Navbar />
 			<div className="container">
-					{!loading && (
-						<div>{pokemon.nome}</div>
-					)}
+				{!loading && (
+					<div>{pokemon.nome}
+						{/* <p>Peso: {atributos.weight} </p>
+						<p>Altura: {atributos.height} </p>
+						<p>{atributos.stats[0].base_stat}hp</p>
+						<p>{atributos.stats[1].base_stat}ataque</p>
+						<p>{atributos.stats[2].base_stat}defesa</p>
+						<p>{atributos.stats[3].base_stat}ataque especial</p>
+						<p>{atributos.stats[4].base_stat}defesa especial</p>
+						<p>{atributos.stats[5].base_stat}velocidade</p> */}
+					</div>
+				)}
 			</div>
 		</div>
 	);
